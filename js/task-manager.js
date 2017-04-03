b5719190d01c331f73b3b95c368dc8f791b02cba
Ireneu Pla <ireneup@gmail.com>
2017-04-04 00:34:51 +0200

    :sparkles: Populate filter by dropdowns

--- js/task-manager.js
+++ js/task-manager.js
@@ -20,10 +20,16 @@ var monthNames = {
     12: 'December'
 };
 
-var monthPanels = {};
+var monthPanels = {}, monthCollapsers = {};
+
+function setHeader(generalData) {
+    document.getElementsByTagName('title')[0].innerHTML = generalData.title;
+    document.getElementById('roadmap-header').innerHTML = generalData.title + ' <small>' + generalData.description + '</small>';
+}
 
 function createMonthPanels() {
     var monthsContainer = $('#months');
+    var filterMonthDropdown = $('#filter-month-dropdown');
     for (var i = 1; i < 13; i++) {
         var monthContainerId = monthNames[i] + i;
         monthsContainer.append(
@@ -39,10 +45,30 @@ function createMonthPanels() {
                     '<div class="panel-body">' +
                     '</div>' +
             '</div>');
+        monthCollapsers[i] = $('#' + monthContainerId);
         monthPanels[i] = $('#' + monthContainerId + '> .panel-body');
+        filterMonthDropdown.append('<li><a href="#" onclick="showMonth(' + i + ')">' + monthNames[i] + '</a></li>');
+    }
+}
+
+function showMonth(month) {
+    for (var i = 1; i < 13; i++) {
+        if (i === month) {
+            monthCollapsers[i].collapse('show');
+        } else {
+            monthCollapsers[i].collapse('hide');
+        }
+    }
+}
+
+function populatePeopleList() {
+    var filterPeopleDropdown = $('#filter-person-dropdown');
+    for (var person in team) {
+        filterPeopleDropdown.append('<li><a href="#" onclick="">' + team[person].name + '</a></li>');
     }
 }
 
 window.onload = function () {
+    setHeader(generalData);
     createMonthPanels();
 };
