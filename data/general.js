e22bdf24f7a79376d1f9b988d639b28753a5fbe1
Ireneu Pla <ireneup@gmail.com>
2017-04-04 00:34:10 +0200

    :bento: Add example data

---
+++ data/general.js
@@ -0,0 +1,23 @@
+var generalData = {
+    title: "Roadmap",
+    description: "<span class=\"glyphicon glyphicon-ok\"></span> A simple interface for a team's or organization's tasks."
+};
+
+var team = {
+    nickname: {
+        color: '#ffd9b3',
+        name: 'Designer'
+    },
+    handle: {
+        color: '#b3ecff',
+        name: 'Salesman'
+    },
+    username: {
+        color: '#b3ffb3',
+        name: 'Marketer'
+    },
+    pseudonym: {
+        color: '#d9b3ff',
+        name: 'SysAdmin'
+    }
+};
---
+++ data/tasks.js
@@ -0,0 +1,28 @@
+var tasksData = [
+    {
+        month: 9,
+        person: team.nickname,
+        title: 'Newsletter design',
+        description: 'Start designing the newsletter template for the following year.'
+    },
+    {
+        month: 2,
+        person: team.handle,
+        title: 'Call resellers',
+        description: 'Call resellers in areas where newsletters received many clicks with special offers. Get the locality rankings for the previous month from the IT department. <br>Offer guidelines according to ranking evolution, ordered volume and current room temperature can be found on the internal server\'s offer folder.',
+        concerned: [team.pseudonym]
+    },
+    {
+        month: 12,
+        person: team.username,
+        title: 'Write newsletter content',
+        description: 'Use your inspiration to write a better description for this task as well.'
+    },
+    {
+        month: 5,
+        date: '30th',
+        person: team.pseudonym,
+        title: 'Get raise',
+        description: 'Go see CEO on the day while he is walking to the helicopter that takes him on his bi-annual 3 month vacation so that he sends you a postcard.'
+    }
+];
